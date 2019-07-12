e5ac91be6238b37fabb3fdd47125898aace6789b
Chris Junker <elevatemedfit@gmail.com>
2019-07-12 14:02:57 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+
+  attr_accessor :word
+  @@matches = []
+
+  def initialization(word)
+    @word = word
+    @@matches << self
+  end
+
+  def self.matches
+    @@matches
+  end
+
+  def match(array)
+    array.all? {|w| w.sort==@word.sort}
+  end
+
+
+end# Your code goes here!
