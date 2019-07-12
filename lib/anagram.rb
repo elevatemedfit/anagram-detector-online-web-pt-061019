4d0197dc89428354df95af6aa28ef7b4897ef4b0
Chris Junker <elevatemedfit@gmail.com>
2019-07-12 14:52:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,12 @@
 class Anagram
-
   attr_accessor :word
-  @@matches = []
 
-  def initialization(word)
+  def initialize(word)
     @word = word
-    @@matches << self
   end
 
-  def self.matches
-    @@matches
+  def match(input)
+    input.map {|w| w.split("").sort == @word.split("").sort}
+
   end
-
-  def match(array)
-    array.all? {|w| w.sort==@word.sort}
-  end
-
-
 end# Your code goes here!
